db04d89411e6b81674de55bb2f58252ef117dbb6
DHANVI SRI VEMURI <vemuri.dhanvisri@gmail.com>
2025-11-29 21:03:10 +0530

    Update Clothes.jsx

--- TRIBECRAFT/src/pages/Clothes.jsx
+++ TRIBECRAFT/src/pages/Clothes.jsx
@@ -7,13 +7,13 @@ const products = [
   { id: 4, name: "Cotton Saree", price: "₹1000", image: "https://www.odishaestore.com/image/cache/sellers/20/White-With-Black-Border-Hand-Woven-Kotpad-Saree-750x750.jpg" },
   { id: 5, name: "Sambalpuri cotton saree", price: "₹4500", image: "https://www.odikala.com/cdn/shop/files/TRI3D__defaultterracotta__silk_set112_juhi_side__2024-9-20-21-20-28__1600X2400_d1e1ffb1-46b0-4a24-84d0-e9d18aca0069_1200x.jpg?v=1726931492" },
   { id: 6, name: "Chakhesang tribe traditional shawl", price: "₹3300", image: "https://i0.wp.com/madeinnagalandcenter.in/wp-content/uploads/2023/07/chakhesang.jpg?fit=600%2C600&ssl=1" },
-  { id: 7, name: "Ethnic Afghan Kuchi Handmade Dress", price: "₹1500", image: "" },
-  { id: 8, name: "Ethnic Afghan Kuchi Handmade Dress", price: "₹1500", image: "" },
-  { id: 9, name: "Ethnic Afghan Kuchi Handmade Dress", price: "₹1500", image: "" },
-  { id: 10, name: "Ethnic Afghan Kuchi Handmade Dress", price: "₹1500", image: "" },
-  { id: 11, name: "Ethnic Afghan Kuchi Handmade Dress", price: "₹1500", image: "" },
-  { id: 12, name: "Ethnic Afghan Kuchi Handmade Dress", price: "₹1500", image: "" },
-    { id: 13, name: "Ethnic Afghan Kuchi Handmade Dress", price: "₹1500", image: "" }
+  { id: 7, name: "Hand Painted Tassar Silk Stole", price: "₹3400", image: "https://www.aadivasi.org/storage/product/images/original/01JE3AGRQVPD5VKBFSSSF7H1S2.jpg" },
+  { id: 8, name: "Toda Hand Embroidery Shawls", price: "₹5165", image: "https://www.gitagged.com/wp-content/uploads/2019/08/TODA-03-SHAWL-BW-P7-01-1.jpg" },
+  { id: 9, name: "Slub Silk Hancrafted stole", price: "₹3500", image: "https://itokri.com/cdn/shop/files/3T1A6762_2ff4145c-4b1e-4b54-97e9-232018c69d7f.jpg?v=1757339469&width=480" },
+  { id: 10, name: "Himalayan Handloom Scarf in Pure Wool", price: "₹5000", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIzYMG01eZQ3BzBjvHgVwOAPi4jSiBfh2xVw&s" },
+  { id: 11, name: "Tribal pattern pure cotton saree", price: "₹3000", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhQMy4AoC5Xdqa5OFBLz1DmBaroBsUiavxHg&s" },
+  { id: 12, name: "OdiKala Sambalpuri Cotton Saree with Blouse Piece", price: "₹14575", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.in%2FOdiKala-Sambalpuri-Cotton-Blouse-Mehendi%2Fdp%2FB0D95JXVJR&psig=AOvVaw1p1tfZ0xXAt4DFrmM-vtp2&ust=1764506489059000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMi-u4yxl5EDFQAAAAAdAAAAABAK" },
+    { id: 13, name: "Kosa Silk Tribal Art Saree With Blue Border", price: "₹5545", image: "https://5.imimg.com/data5/ECOM/Default/2024/6/429327222/BH/KQ/QL/163930577/img-1012-500x500.jpg" }
 ];
 const Clothes = () => (
   <div className="products-container">
